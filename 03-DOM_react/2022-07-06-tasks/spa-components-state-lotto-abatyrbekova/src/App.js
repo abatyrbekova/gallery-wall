c36ad3736dc2834d7827694a42deb12a75c73719
abatyrbekova <batyrbekova592@gmail.com>
2022-07-31 22:06:42 +0200

    task finished

---
+++ 03-DOM_react/2022-07-06-tasks/spa-components-state-lotto-abatyrbekova/src/App.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function App() {
+  const [numsArr, setNumsArr] = useState([]);
+
+  const handleClick = () => {
+    const newArr = [];
+
+    while (newArr.length < 6) {
+      const randomNum = Math.floor(Math.random() * 49 + 1);
+
+      // if (!newArr.includes(randomNum)) {
+      //   newArr.push(randomNum);
+      // }
+
+      newArr.push(randomNum);
+      new Set([...newArr]);
+    }
+
+    const superNum = Math.floor(Math.random() * 10 + 1);
+    newArr.push(superNum);
+
+    setNumsArr(newArr);
+  };
+
+  return (
+    <div className="App">
+
+      {/* {console.log(numsArr)} */}
+
+      <div className="container">
+        {numsArr.map((item, i) => (
+          <div className="num" key={i}> {item} </div>
+        ))}
+      </div>
+
+      <div>
+        <button onClick={() => setNumsArr([])}> Reset </button>
+        <button onClick={handleClick}> get Lucky Nums </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
